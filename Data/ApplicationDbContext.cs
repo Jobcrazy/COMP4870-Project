bf8249b5322685ae02ec4ff66ab056ee9ddd2b32
LiuHang <liuhang@kaka888.com>
2022-03-04 17:37:23 -0800

    1. Disable HTTPS 2. Use DbConext instead of IdentityDbCOntext
    Signed-off-by: LiuHang <liuhang@kaka888.com>

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using backend.Models;
 
 namespace backend.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
